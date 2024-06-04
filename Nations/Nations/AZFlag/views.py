8c35f45a400376d912085b003fd34a3c91d65892
= <=>
2024-06-04 06:30:54 +0530

    ITER1 complete
    Display all the nations list

--- Nations/Nations/AZFlag/views.py
+++ Nations/Nations/AZFlag/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render # type: ignore
-from .models import User
+from django.shortcuts import render
+from .models import User, Flag
+from django.http import HttpResponse
 
 """
 screen page (main)
 """
 # function index
 def index(request):
+  f = open('media/Nations/files/nations.txt', 'r')
+  nations = []
+  line = f.readline()
+  while line != "":
+    nations.append(line) 
+    line = f.readline()
+  f.close()
   if (request.method == "POST"):
     pass
   else:
-    return render(request, "index.html")
+    return render(request, "AZFlag/index.html", {'data': nations})
