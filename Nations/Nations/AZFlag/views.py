b7be63eb4cfcc0501428aade2f7b70656d668679
Siddharth Bhatore <=>
2024-06-11 05:01:24 +0530

    modify views.py: ITER2

--- Nations/Nations/AZFlag/views.py
+++ Nations/Nations/AZFlag/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render
 from .models import Flag
 from django.http import HttpResponse
+from Authenticator.models import Person
 
 """
 screen page (main)
 """
 # function index
 def index(request):
-  f = open('media/Nations/files/nations.txt', 'r')
   nations = []
-  line = f.readline()
-  count = 0
-  while line != "":
-    count += 1
-    flag = Flag(countryName=line, imgURL=("imgURL" + str(count) + ".png"))
-    flag.save() 
-    nations.append(line)
+  if (len(Flag.objects.all()) < 1):
+    f = open('media/Nations/files/nations.txt', 'r')
     line = f.readline()
-  f.close()
+    count = 0
+    while line != "":
+      count += 1
+      flag = Flag(countryName=line, imgURL=("imgURL" + str(count) + ".png"))
+      flag.save() 
+      nations.append(line)
+      line = f.readline()
+    f.close()
+  else:
+    flags = Flag.objects.all()
+    for flag in flags:
+      nations.append(flag.countryName)
   if (request.method == "POST"):
     pass
   else:
